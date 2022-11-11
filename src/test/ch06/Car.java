d6ed8c5b200dd727c8a18ea52ab11a7a7d972a6e
eunbiro <ebee91@naver.com>
2022-11-11 17:08:59 +0900

    up

---
+++ src/test/ch06/Car.java
@@ -0,0 +1,33 @@
+package test.ch06;
+
+public class Car {
+	// 1. 필드 : 객체의 데이터를 저장
+	String company = "현대자동차";
+	String model = "그렌저";
+	String color = "검정";
+	int maxSpeed = 350;
+	int speed;
+//	boolean start;	// 시동여부
+	
+	// 자바에서는 생성자를 명시하지 않으면 컴파일 과정에서 생성자를 생성해준다.
+	
+	Car(String model) {
+//		this.model = model;
+		this(model, "은색", 250);
+	}
+	Car(String model, String color) {
+//		this.model = model;
+//		this.color = color;
+		this(model, color, 250);
+	}
+	
+	Car(String model, String color, int maxSpeed) {
+		this.model = model;
+		this.color = color;
+		this.maxSpeed = maxSpeed;
+	}
+	
+
+
+ 
+}
