590143d598f9ebe63ca76b8cf3917d2e03ca41f0
eunbiro <ebee91@naver.com>
2022-11-17 18:02:24 +0900

    1117

--- src/test/ch07/Computer.java
+++ src/test/ch07/Computer.java
@@ -5,8 +5,8 @@ public class Computer extends Calculator {
 	// 오버라이드 : 부모클래스의 메소드를 재 정의(반환타입, 메소드 이름, 매개변수가 같아야 한다.)
 	@Override // 오른클릭 > source > override
 	public double areaCircle(double r) {
-		// TODO Auto-generated method stub
-		return super.areaCircle(r);
+		System.out.println("Computer 객체의 areaCircle() 실행");
+		return Math.PI * r * r;
 	}
 
 }
